51091c6043ee1e05278123839cb1db8ff604a2fc
Andres Montano <amontano@virginia.edu>
2013-07-15 17:08:11 -0600

    adding asset

--- app/assets/javascripts/subjects_integration/application.js
+++ app/assets/javascripts/subjects_integration/application.js
@@ -10,7 +10,6 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-// Old used thickbox-compressed and subjects_integration/category_selector
 //= require subjects_integration/jquery.checktree
 //= require subjects_integration/model-searcher
 //= require subjects_integration/jquery.draggable.popup
--- lib/subjects_integration/engine.rb
+++ lib/subjects_integration/engine.rb
@@ -1,4 +1,7 @@
 module SubjectsIntegration
   class Engine < ::Rails::Engine
+    initializer :assets do |config|
+      Rails.application.config.assets.precompile.concat(['subjects_integration/jquery.draggable.popup.js'])
+    end
   end
 end
