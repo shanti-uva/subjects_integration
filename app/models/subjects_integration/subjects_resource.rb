d1203bc11cd27ebef89c3b33d17b572fffb1d102
Andres Montano <amontano@virginia.edu>
2013-11-25 14:40:48 -0600

    pointing subjects to shanti instead of thlib

--- app/models/subjects_integration/subjects_resource.rb
+++ app/models/subjects_integration/subjects_resource.rb
@@ -5,17 +5,17 @@ module SubjectsIntegration
     case InterfaceUtils::Server.environment
     when InterfaceUtils::Server::DEVELOPMENT
       self.site = 'http://127.0.0.1/'
-      headers['Host'] = "dev-subjects.#{InterfaceUtils::Server.get_domain}"
+      headers['Host'] = 'dev-subjects.kmaps.virginia.edu' #"dev-subjects.#{InterfaceUtils::Server.get_domain}"
     when InterfaceUtils::Server::STAGING
       self.site = 'http://127.0.0.1/'
-      headers['Host'] = "staging.subjects.#{InterfaceUtils::Server.get_domain}"
+      headers['Host'] = 'staging.kmaps.virginia.edu' #"staging.subjects.#{InterfaceUtils::Server.get_domain}"
     when InterfaceUtils::Server::PRODUCTION
       self.site = 'http://127.0.0.1/'
-      headers['Host'] = "subjects.#{InterfaceUtils::Server.get_domain}"
+      headers['Host'] = 'subjects.kmaps.virginia.edu' #"subjects.#{InterfaceUtils::Server.get_domain}"
     when InterfaceUtils::Server::LOCAL
       self.site = 'http://localhost/shanti/subjects/'
     else
-      self.site = "http://subjects.#{InterfaceUtils::Server.get_domain}/"
+      self.site = 'http://subjects.kmaps.virginia.edu' #"http://subjects.#{InterfaceUtils::Server.get_domain}/"
     end
 
     self.timeout = 100
