87e6c47a304e68e198953d2f5995648aa7f52459
Andres Montano <amontano@virginia.edu>
2015-01-30 23:43:18 -0600

    adding calculation of main caption to api

--- app/models/subjects_integration/feature.rb
+++ app/models/subjects_integration/feature.rb
@@ -3,6 +3,10 @@ module SubjectsIntegration
     acts_as_active_resource_family_tree
     
     headers['Host'] = SubjectsResource.headers['Host'] if !SubjectsResource.headers['Host'].blank?
+    
+    def caption
+      current_lang_code = Language.current.code
+      self.nested_captions.detect{|c| c.language==current_lang_code}
+    end
   end
-  
 end
