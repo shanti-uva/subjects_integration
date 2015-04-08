750839fafd23d50810bb76d7f4d06719c5ca5b6a
Andres Montano <amontano@virginia.edu>
2015-04-07 23:35:43 -0600

    adding find by name to active resource

--- app/models/subjects_integration/feature.rb
+++ app/models/subjects_integration/feature.rb
@@ -8,5 +8,10 @@ module SubjectsIntegration
       current_lang_code = Language.current.code
       self.nested_captions.detect{|c| c.language==current_lang_code}
     end
+    
+    def self.find_by_name(name)
+      f = Feature.get("by_name/#{URI::encode(name)}").first
+      f.nil? ? nil : self.find(f['id'])
+    end
   end
 end
