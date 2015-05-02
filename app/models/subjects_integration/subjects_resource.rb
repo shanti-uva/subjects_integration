be82890eec0fd5d0f846bf68c2d148c21d200b35
Andres Montano <amontano@virginia.edu>
2015-05-02 01:11:38 -0600

    updating staging url for subjects

--- app/models/subjects_integration/subjects_resource.rb
+++ app/models/subjects_integration/subjects_resource.rb
@@ -8,7 +8,7 @@ module SubjectsIntegration
       headers['Host'] = 'dev-subjects.kmaps.virginia.edu' #"dev-subjects.#{InterfaceUtils::Server.get_domain}"
     when InterfaceUtils::Server::STAGING
       self.site = 'http://127.0.0.1/'
-      headers['Host'] = 'staging.kmaps.virginia.edu' #"staging.subjects.#{InterfaceUtils::Server.get_domain}"
+      headers['Host'] = 'staging-subjects.kmaps.virginia.edu' #"staging-subjects.#{InterfaceUtils::Server.get_domain}"
     when InterfaceUtils::Server::PRODUCTION
       self.site = 'http://127.0.0.1/'
       headers['Host'] = 'subjects.kmaps.virginia.edu' #"subjects.#{InterfaceUtils::Server.get_domain}"
