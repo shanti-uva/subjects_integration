98f5c5143fa35b2154f5347721ec9f09f8b575f7
Andres Montano <amontano@virginia.edu>
2020-04-30 02:35:07 -0600

    adding trailing slash to active resource urls

--- app/models/subjects_integration/subjects_resource.rb
+++ app/models/subjects_integration/subjects_resource.rb
@@ -4,15 +4,15 @@ module SubjectsIntegration
     
     case InterfaceUtils::Server.environment
     when InterfaceUtils::Server::DEVELOPMENT
-      self.site = 'http://dev-subjects.kmaps.virginia.edu' #"dev-subjects.#{InterfaceUtils::Server.get_domain}"
+      self.site = "http://dev-subjects.#{InterfaceUtils::Server.get_domain}/"
     when InterfaceUtils::Server::STAGING
-      self.site = 'http://staging-subjects.kmaps.virginia.edu' #"staging-subjects.#{InterfaceUtils::Server.get_domain}"
+      self.site = "http://staging-subjects.#{InterfaceUtils::Server.get_domain}/"
     when InterfaceUtils::Server::PRODUCTION
-      self.site = 'http://subjects.kmaps.virginia.edu' #"subjects.#{InterfaceUtils::Server.get_domain}"
+      self.site = "http://subjects.#{InterfaceUtils::Server.get_domain}/"
     when InterfaceUtils::Server::LOCAL
-      self.site = 'http://localhost/shanti/subjects/'
+      self.site = "http://localhost/shanti/subjects/"
     else
-      self.site = 'http://subjects.kmaps.virginia.edu' #"http://subjects.#{InterfaceUtils::Server.get_domain}/"
+      self.site = "http://subjects.#{InterfaceUtils::Server.get_domain}/"
     end
 
     self.timeout = 100
