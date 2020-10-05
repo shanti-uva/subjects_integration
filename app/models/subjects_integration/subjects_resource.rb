10b4f24143649bb365e77f45c3a5daf180e9956a
Andres Montano <amontano@virginia.edu>
2020-10-05 12:51:44 -0500

    hard-coding subjects to kmaps.virginia.edu given that there is no other subjects anymore

--- app/models/subjects_integration/subjects_resource.rb
+++ app/models/subjects_integration/subjects_resource.rb
@@ -4,15 +4,15 @@ module SubjectsIntegration
     
     case InterfaceUtils::Server.environment
     when InterfaceUtils::Server::DEVELOPMENT
-      self.site = "http://dev-subjects.#{InterfaceUtils::Server.get_domain}/"
+      self.site = "http://dev-subjects.kmaps.virginia.edu/"
     when InterfaceUtils::Server::STAGING
-      self.site = "http://staging-subjects.#{InterfaceUtils::Server.get_domain}/"
+      self.site = "http://staging-subjects.kmaps.virginia.edu/"
     when InterfaceUtils::Server::PRODUCTION
-      self.site = "http://subjects.#{InterfaceUtils::Server.get_domain}/"
+      self.site = "http://subjects.kmaps.virginia.edu/"
     when InterfaceUtils::Server::LOCAL
       self.site = "http://localhost/shanti/subjects/"
     else
-      self.site = "http://subjects.#{InterfaceUtils::Server.get_domain}/"
+      self.site = "http://subjects.kmaps.virginia.edu/"
     end
 
     self.timeout = 100
