8e58c4ad288070472cc162fda1a0a5b755dfea6f
Andres Montano <amontano@virginia.edu>
2023-03-18 16:03:38 -0600

    fixing search caching

--- app/models/subjects_integration/feature.rb
+++ app/models/subjects_integration/feature.rb
@@ -3,12 +3,6 @@ module SubjectsIntegration
     acts_as_active_resource_family_tree
     acts_as_indexable uid_prefix: 'subjects'
     
-    def search(id)
-      Rails.cache.fetch("subjects_integration/feature/#{id}", expires_in: 1.hour) do
-        self.flare_search(id)
-      end
-    end
-        
     def caption
       current_lang_code = Language.current.code
       self.nested_captions.detect{|c| c.language==current_lang_code}
@@ -17,6 +11,12 @@ module SubjectsIntegration
     def self.find_by_name(name)
       f = Feature.get("by_name/#{URI::encode(name)}").first
       f.nil? ? nil : self.find(f['id'])
+    end
+    
+    def self.search(id)
+      Rails.cache.fetch("subjects_integration/feature/#{id}", expires_in: 1.hour) do
+        self.flare_search(id)
+      end
     end
   end
 end
